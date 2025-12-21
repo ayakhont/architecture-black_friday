268d889dc198a2ee98b21ffdb12a22e711559040
ayakhont <ayakhont@gmail.com>
2025-12-21 04:07:07 +0100

    task 2 debug

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -1,11 +1,60 @@
 #!/bin/bash
 
-###
-# Инициализируем бд
-###
-
-docker compose exec -T mongodb1 mongosh <<EOF
-use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+# Инициализируем конфигурационный сервер
+docker exec -i configSrv mongosh --port 27017 --quiet <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
 EOF
 
+# Инициализируем первый шард
+docker exec -i shard1 mongosh --port 27018 --quiet <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+      ]
+    }
+);
+EOF
+
+# Инициализируем второй шард
+docker exec -i shard2 mongosh --port 27019 --quiet <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 1, host : "shard2:27019" }
+      ]
+    }
+  );
+EOF
+
+# Инициализируем роутер, добавляем шарды, создаем бд и коллекцию с шардированием
+docker exec -i mongos_router mongosh --port 27020 --quiet <<EOF
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+db.helloDoc.countDocuments()
+EOF
+
+# Проверяем что данные распределились по шардам
+docker exec -i shard1 mongosh --port 27018 --quiet <<EOF
+use somedb;
+db.helloDoc.countDocuments();
+EOF
+
+docker exec -i shard2 mongosh --port 27019 --quiet <<EOF
+use somedb;
+db.helloDoc.countDocuments();
+EOF
